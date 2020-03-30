6d5de4e35da70511dffee02094d1aa4003bae727
talianassi921 <talianassi@gmail.com>
2020-03-30 16:16:46 -0700

    added feature flag capabilities

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,4 +1,5 @@
 ﻿import React, { Component } from "react";
+import { SplitTreatments, withSplitFactory } from "@splitsoftware/splitio-react"
 import TodoItems from "./TodoItems";
 import "./TodoList.css";
 
@@ -9,7 +10,6 @@ class TodoList extends Component {
         this.state = {
             items: []
         };
-
         this.addItem = this.addItem.bind(this);
         this.deleteItem = this.deleteItem.bind(this);
     }
@@ -47,20 +47,42 @@ class TodoList extends Component {
 
     render() {
         return (
+
+            <SplitTreatments names={['talia_todolist_delete']} >
+                {({ treatments }) => {
+                    return this.renderContent(treatments['talia_todolist_delete']) 
+                }}
+
+            </SplitTreatments>
+        );
+    }
+
+    renderContent(treatment) {
+        const buttonClass = treatment === 'on' ? '' : 'button_new';
+
+        return (
             <div className="todoListMain">
                 <div className="header">
                     <form onSubmit={this.addItem}>
                         <input ref={(a) => this._inputElement = a}
                             placeholder="Enter Task">
                         </input>
-                        <button type="submit">Add</button>
+                        <button className={buttonClass} type="submit">Add</button>
                     </form>
                 </div>
                 <TodoItems entries={this.state.items}
                     delete={this.deleteItem} />
             </div>
-        );
+            )
     }
 }
 
-export default TodoList;
+const sdkConfig = {
+    core: {
+        authorizationKey: 's2959s3memm2hp1b03u34khu1sjl0106j6qr',
+        key: 'talia.nassi@split.io'
+    },
+    debug: true
+};
+
+export default withSplitFactory(sdkConfig)(TodoList);
