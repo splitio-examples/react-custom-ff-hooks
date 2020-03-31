4319eff95fff103c1ce5cc0c3eb37d6cfab35a2b
talianassi921 <talianassi@gmail.com>
2020-03-31 12:10:23 -0700

    final todo

--- src/TodoList.js
+++ src/TodoList.js
@@ -45,20 +45,27 @@ class TodoList extends Component {
         });
     }
 
+    //React render function to render the todo list
     render() {
         return (
-
+            //Split Treatments is a React component that performs feature evalutation
+            //Does a call to client.getTreatmentswithConfig(names)
+            //You pass in the name of the feature flag
+            //This returns a treatment and a config
             <SplitTreatments names={['talia_todolist_delete']} >
                 {({ treatments }) => {
                     return this.renderContent(treatments['talia_todolist_delete']) 
                 }}
-
             </SplitTreatments>
         );
     }
 
+    // As a better convention, I modularized the code
     renderContent(treatment) {
-        const buttonClass = treatment === 'on' ? '' : 'button_new';
+        // Variable that differentiates between treatment on and off
+        // If treatment is on, we are setting buttonClass to 'button_new'
+        // If treatment is off, we are setting buttonClass to 'button_default'
+        const buttonClass = treatment.treatment === 'on' ? 'button_new' : 'button_default';
 
         return (
             <div className="todoListMain">
@@ -67,7 +74,8 @@ class TodoList extends Component {
                         <input ref={(a) => this._inputElement = a}
                             placeholder="Enter Task">
                         </input>
-                        <button className={buttonClass} type="submit">Add</button>
+                        {/* Adds the correct class name to the button */}
+                        <button className={buttonClass} type="submit">Add</button> 
                     </form>
                 </div>
                 <TodoItems entries={this.state.items}
@@ -77,12 +85,17 @@ class TodoList extends Component {
     }
 }
 
+// The config that I use to configure my split instance
+// This initializes the Split Factory, which is the entry point of the library
+// From Syntax in UI
 const sdkConfig = {
     core: {
         authorizationKey: 's2959s3memm2hp1b03u34khu1sjl0106j6qr',
         key: 'talia.nassi@split.io'
     },
+    // Makes the console display all info
     debug: true
 };
 
+// The SplitFactor Component uses the config above
 export default withSplitFactory(sdkConfig)(TodoList);
